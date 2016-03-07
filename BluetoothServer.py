b89e2aedbae0246ea2ab88a238c58c8ddd7dc0af
Bill Odefey <wodefey@live.com>
2016-03-07 10:43:06 -0600

    Initial BuetoothServer

---
+++ BluetoothServer.py
@@ -0,0 +1,20 @@
+import socket
+
+hostMACAddress = '00:00:00:00:5a:ad'
+port = 3
+backlog = 1
+size = 1024
+s = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
+s.bind((hostMACAddress,port))
+s.listen(backlog)
+try:
+    client, address = s.accept()
+    while 1:
+        data = client.recv(size)
+        if data:
+            print(data)
+            client.send(data)
+except:
+    print("closing socket")
+    client.close
+    s.close
